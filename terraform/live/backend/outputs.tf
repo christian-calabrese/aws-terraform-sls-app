0b1c678868ae37e28f118831150373548f3ca123
christian-calabrese <christian.calabrese@outlook.com>
2024-01-18 00:58:27 +0100

    feature(backend): backend workspace is now returning the api endpoint url

--- terraform/live/backend/outputs.tf
+++ terraform/live/backend/outputs.tf
@@ -1,4 +1,7 @@
 ################################################################################
-# Frontend
+# Backend
 ################################################################################
-
+output "api_endpoint" {
+  description = "The endpoint of the REST API"
+  value       = aws_api_gateway_stage.notes_api.invoke_url
+}
