b0bc16aad1215cd438b9d8054192e80e85c885fd
christian-calabrese <christian.calabrese@outlook.com>
2024-01-18 23:43:06 +0100

    fix(backend): added stage arn output

--- terraform/live/backend/outputs.tf
+++ terraform/live/backend/outputs.tf
@@ -5,3 +5,8 @@ output "api_endpoint" {
   description = "The endpoint of the REST API"
   value       = aws_api_gateway_stage.notes_api.invoke_url
 }
+
+output "api_stage_arn" {
+  description = "The arn of the REST API stage"
+  value       = aws_api_gateway_stage.notes_api.arn
+}
