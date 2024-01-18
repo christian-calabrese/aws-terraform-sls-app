0747f81a3d46715b9dafe62016fedad13dff19fc
christian-calabrese <christian.calabrese@outlook.com>
2024-01-18 02:44:15 +0100

    feature(backend): added logging to apigateway stage

--- terraform/live/backend/main.tf
+++ terraform/live/backend/main.tf
@@ -149,6 +149,44 @@ resource "aws_lambda_function" "delete_note" {
 ################################################################################
 # API Gateway
 ################################################################################
+resource "aws_api_gateway_account" "main" {
+  cloudwatch_role_arn = aws_iam_role.main.arn
+}
+
+resource "aws_iam_role" "main" {
+  name = "api-gateway-logs-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = aws_iam_role.main.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+resource "aws_api_gateway_method_settings" "notes_api" {
+  rest_api_id = aws_api_gateway_rest_api.notes_api.id
+  stage_name  = aws_api_gateway_stage.notes_api.stage_name
+  method_path = "*/*"
+  settings {
+    logging_level = "ERROR"
+  }
+}
 
 resource "aws_api_gateway_rest_api" "notes_api" {
   name        = "${var.project}-${var.environment}-notes-api"
