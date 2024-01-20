bed45f6c493c683a3951f4e6c071fcefbbd86de9
christian-calabrese <christian.calabrese@outlook.com>
2024-01-20 16:41:41 +0100

    fix(workspaces): backend-cicd workspace can now read backend workspace outputs

--- terraform/tfc/workspaces.tf
+++ terraform/tfc/workspaces.tf
@@ -135,7 +135,8 @@ module "sls-app-backend-eu-south-1-prod" {
 
   remote_state_consumer_ids = [
     module.sls-app-frontend-eu-south-1-prod.id,
-    module.sls-app-waf-eu-south-1-prod.id
+    module.sls-app-waf-eu-south-1-prod.id,
+    module.sls-app-backend-cicd-eu-south-1-prod.id
   ]
 
   environment_sensitive_variables = {
